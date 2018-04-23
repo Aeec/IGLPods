22612c4cfeb50fbf506800e6df924cbc0c9f4cad
Isay Gonzalez <isay.gonzalez@netberry.es>
2018-04-23 12:56:55 +0200

    config pod

---
+++ IGLPod/IGLPod/IGLPod.h
@@ -0,0 +1,19 @@
+//
+//  IGLPod.h
+//  IGLPod
+//
+//  Created by Isay González Lázaro on 23/4/18.
+//  Copyright © 2018 Isay González Lázaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IGLPod.
+FOUNDATION_EXPORT double IGLPodVersionNumber;
+
+//! Project version string for IGLPod.
+FOUNDATION_EXPORT const unsigned char IGLPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGLPod/PublicHeader.h>
+
+
---
+++ IGLPod/Pods/Target Support Files/Pods-IGLPod/Pods-IGLPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IGLPod : NSObject
+@end
+@implementation PodsDummy_Pods_IGLPod
+@end
